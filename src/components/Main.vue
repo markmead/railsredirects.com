ae7e13747280a6736af1bd1ff7ae305f27bde2ee
markmead <itsmarkmead@gmail.com>
2020-09-17 17:49:42 +0100

    mobile tweaks

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -4,34 +4,36 @@
     <Title />
     <Upload @action="parseFile" />
 
-    <div class="flex items-center justify-between mt-8">
+    <div
+      class="flex flex-col items-center mt-8 space-y-4 md:items-center md:justify-between md:flex-row md:space-y-0"
+    >
       <div class="flex items-center space-x-4">
         <input
           type="text"
           v-model="strip"
-          class="p-6 text-lg placeholder-gray-400 transition-colors duration-150 ease-in-out bg-gray-200 border-0 rounded-lg form-input hover:bg-cool-gray-200"
+          class="w-2/3 p-6 text-lg placeholder-gray-400 transition-colors duration-150 ease-in-out bg-gray-200 border-0 rounded-lg form-input hover:bg-cool-gray-200"
           placeholder="TEXT TO REMOVE"
         />
         <input
           type="text"
           v-model="status"
-          class="w-48 p-6 text-lg placeholder-gray-400 transition-colors duration-150 ease-in-out bg-gray-200 border-0 rounded-lg form-input hover:bg-cool-gray-200"
+          class="w-1/3 p-6 text-lg placeholder-gray-400 transition-colors duration-150 ease-in-out bg-gray-200 border-0 rounded-lg form-input hover:bg-cool-gray-200"
           placeholder="STATUS"
         />
-        <button type="button" @click="help = true" class="text-sm text-gray-700 underline uppercase hover:no-underline">
-          How it works
-        </button>
       </div>
-      <div class="space-x-4">
+      <div class="flex items-center space-x-4">
+        <button
+          type="button"
+          @click="help = true"
+          class="order-first text-sm text-gray-700 underline uppercase hover:no-underline"
+        >How it works</button>
         <button
           type="button"
           class="px-12 py-5 text-lg leading-none text-white uppercase transition-shadow duration-150 ease-in-out bg-green-600 rounded-lg hover:shadow-xl"
           v-clipboard:copy="generatedRedirects"
           v-clipboard:success="handleCopySuccess"
           v-clipboard:error="handleCopyError"
-        >
-          Copy Code
-        </button>
+        >Copy Code</button>
       </div>
     </div>
 
